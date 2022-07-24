746affbea9464b0e6ac694cb0968ab977a661e70
Seif Wu <seif.wuxf@gmail.com>
2022-07-24 23:28:50 +0800

    fix(ui): 修复 sx 无法覆盖样式错误

--- packages/ui/src/Typography/TypographyRoot.tsx
+++ packages/ui/src/Typography/TypographyRoot.tsx
@@ -22,13 +22,10 @@ export interface TypographyProps extends HTMLAttributes<HTMLElement> {
   sx?: CSSProperties | ((theme: Theme) => CSSProperties);
 }
 
-const innerCls = genInnerCls('typography');
-
 const TypographyRoot = styled('span', {
   shouldForwardProp: (prop) => isPropValid(prop) && prop !== 'type' && prop !== 'sx',
 })<TypographyProps>((props) => {
   const { type = '', sx, theme: outerTheme } = props;
-  const cls = type && `${innerCls}-${type}`;
   const theme = createTheme(outerTheme)[outerTheme.mode || 'light'];
 
   return {
@@ -36,83 +33,63 @@ const TypographyRoot = styled('span', {
     clear: 'both',
     color: theme?.text?.primary,
     ...(type === 'h1' && {
-      [`&.${cls}`]: {
-        margin: '0px 0px 0.35em',
-        fontWeight: 500,
-        fontSize: 38,
-        lineHeight: 1.23,
-      },
+      margin: '0px 0px 0.35em',
+      fontWeight: 500,
+      fontSize: 38,
+      lineHeight: 1.23,
     }),
     ...(type === 'h2' && {
-      [`&.${cls}`]: {
-        margin: '0px 0px 0.35em',
-        fontWeight: 500,
-        fontSize: 30,
-        lineHeight: 1.35,
-      },
+      margin: '0px 0px 0.35em',
+      fontWeight: 500,
+      fontSize: 30,
+      lineHeight: 1.35,
     }),
     ...(type === 'h3' && {
-      [`&.${cls}`]: {
-        margin: '0px 0px 0.35em',
-        fontWeight: 500,
-        fontSize: 24,
-        lineHeight: 1.35,
-      },
+      margin: '0px 0px 0.35em',
+      fontWeight: 500,
+      fontSize: 24,
+      lineHeight: 1.35,
     }),
     ...(type === 'h4' && {
-      [`&.${cls}`]: {
-        margin: '0px 0px 0.35em',
-        fontWeight: 500,
-        fontSize: 20,
-        lineHeight: 1.35,
-      },
+      margin: '0px 0px 0.35em',
+      fontWeight: 500,
+      fontSize: 20,
+      lineHeight: 1.35,
     }),
     ...(type === 'h5' && {
-      [`&.${cls}`]: {
-        margin: '0px 0px 0.35em',
-        fontWeight: 500,
-        fontSize: 20,
-        lineHeight: 1.35,
-      },
+      margin: '0px 0px 0.35em',
+      fontWeight: 500,
+      fontSize: 20,
+      lineHeight: 1.35,
     }),
     ...(type === 'h6' && {
-      [`&.${cls}`]: {
-        margin: '0px 0px 0.35em',
-        fontWeight: 500,
-        fontSize: 16,
-        lineHeight: 1.35,
-      },
+      margin: '0px 0px 0.35em',
+      fontWeight: 500,
+      fontSize: 16,
+      lineHeight: 1.35,
     }),
     ...(type === 'secondary' && {
-      [`&.${cls}`]: {
-        color: theme?.text?.secondary,
-      },
+      color: theme?.text?.secondary,
     }),
     ...(type === 'code' && {
-      [`&.${cls}`]: {
-        margin: '0 1px',
-        padding: '0.2em 0.4em',
-        fontSize: '0.9em',
-        background: '#f2f4f5',
-        border: '1px solid rgba(0, 0, 0, 0.06)',
-        borderRadius: 3,
-      },
+      margin: '0 1px',
+      padding: '0.2em 0.4em',
+      fontSize: '0.9em',
+      background: '#f2f4f5',
+      border: '1px solid rgba(0, 0, 0, 0.06)',
+      borderRadius: 3,
     }),
     ...(type === 'title' && {
-      [`&.${cls}`]: {
-        margin: '0',
-        lineHeight: 1.23,
-        fontSize: '44px',
-        fontWeight: 600,
-      },
+      margin: '0',
+      lineHeight: 1.23,
+      fontSize: '44px',
+      fontWeight: 600,
     }),
     ...(type === 'lgTitle' && {
-      [`&.${cls}`]: {
-        margin: '0',
-        lineHeight: '1.15',
-        fontSize: '4rem',
-        fontWeight: 'bold',
-      },
+      margin: '0',
+      lineHeight: '1.15',
+      fontSize: '4rem',
+      fontWeight: 'bold',
     }),
     ...sx,
     ...props.style,
