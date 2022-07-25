fd96927bbd4378c3064f06497074c21fa3746e62
Seif Wu <seif.wuxf@gmail.com>
2022-07-25 17:24:50 +0800

    fix(system): 修复 useSx 创建主题时错误

--- packages/system/src/theme/mergeTheme.ts
+++ packages/system/src/theme/mergeTheme.ts
@@ -1,6 +1,7 @@
 import defaultsDeep from 'lodash.defaultsdeep';
 import defaultTheme from './defaultTheme';
 
+// 包含了系统默认主题
 export default function mergeTheme(theme: any, ...args: any[]) {
   const mergedTheme = defaultsDeep(theme, ...args, defaultTheme);
 
--- packages/system/src/theme/useSx.ts
+++ packages/system/src/theme/useSx.ts
@@ -1,12 +1,11 @@
 import { useTheme } from '@emotion/react';
 import type { CSSProperties } from 'react';
-import defaultTheme from './defaultTheme';
 import mergeTheme from './mergeTheme';
 import { Theme } from './types';
 
 export default function useSx(sx: CSSProperties | ((theme: Theme) => CSSProperties) | undefined) {
   const themeCtx = useTheme();
-  const theme = mergeTheme(defaultTheme, themeCtx);
+  const theme = mergeTheme(themeCtx);
 
   return typeof sx === 'function' ? sx(theme) : sx;
 }
