eba78f863477c88a1dfbc22cd248729df34c797c
Seif Wu <seif.wuxf@gmail.com>
2022-08-11 11:56:59 +0800

    chore(hooks): remove useFullscreen

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,3 +1,2 @@
 export { default as useControlled } from './useControlled';
 export { default as useLocalStorage } from './useLocalStorage';
-export { default as useFullscreen } from './useFullscreen';
